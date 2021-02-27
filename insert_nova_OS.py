97d22df3854e05990c554440a05c65c3f77a3fc0
sistemadevsys <infodevsys@yahoo.com.br>
2021-02-27 09:59:27 -0300

    Atualizado para inserir nova OS

--- insert_nova_OS.py
+++ insert_nova_OS.py
@@ -8,6 +8,7 @@ import mysql.connector
 import re
 from datetime import datetime
 
+
 try:
     # Mysql Local
     con_mysql = mysql.connector.connect(
@@ -54,17 +55,32 @@ try:
     # Precisa ter pelo menos dois campos para passar para string
     t_mysql_fun = cursor_mysql.fetchall()
 
-    dt_ = datetime.now().strftime('%Y-%m-%d %H:%M')
-
+    list_rfire = []
     for rfire, rfun, desc, respo, dt_a, dt_p,  vl, eq in t_ser:
-        # print(rfun)
-        # for rfos, rfun_os, descri, dt, vl_os, user_id in t_os:
-        # Se Tabela t_os acima estiver vazia não entra aqui.
-        for rfu, nome, u_fun_id in t_mysql_fun:  # Funcionario
-            # TODO:
-            # Primeira vez para inserir e depois mudar e comparar ultimo
-            # referencial da OS banco local (2 for com todas OS)
-            if rfu == rfun:
+        if rfun == 1 or rfun == 2 or rfun == 5 or rfun == 19 or rfun == 22:
+            # print(rfire, desc)
+            list_rfire.append(rfire)
+
+    list_rfos = []
+    for rfos, rfun_os, descri, dt, vl_os, user_id in t_os:
+        list_rfos.append(rfos)
+
+    new_list = []
+    for element in list_rfire:
+        if element not in list_rfos:
+            new_list.append(element)
+    print('''Lista de referenciais diferentes dos bancos
+            (falta inserir no site): \n''', new_list)
+
+    print(f'Último referencial serviços local: {new_list[-1]}')
+    # list_rfos[-2] -1 nula...
+    print(f'Último referencial serviços (OS) site: {list_rfos[-1]}')
+
+    # comparar e fazer insert dos referenciais diferentes...
+    dt_ = datetime.now().strftime('%Y-%m-%d %H:%M')
+    for rfire, rfun, desc, respo, dt_a, dt_p, vl, eq in t_ser:
+        for rfos, rfun_os, descri, dt, vl_os, user_id in t_os:
+            if rfire == new_list[-1] and rfun == rfun_os:
                 if desc is None:
                     desc = "Sem descrição."
                 desc = re.sub(r"^\s+|\s+$", "", desc)
@@ -73,6 +89,7 @@ try:
                 eq = re.sub(r"^\s+|\s+$", "", eq)
                 if respo is None:
                     respo = "Sem responsavel."
+                respo = re.sub(r"^\s+|\s+$", "", respo)
                 if dt_a is None or dt_a == "":
                     dt_a = '1111-11-11 11:11:11 1.1'
                 if dt_p is None or dt_p == "":
@@ -87,33 +104,14 @@ try:
                             %s, %s, %s, %s, %s)"""
                 val = (
                     rfire, rfun, desc, respo, dt_a, dt_p, dt_, dt_,
-                    vl, eq, 0, 0, u_fun_id
+                    vl, eq, 0, 0, user_id
                     )
                 cursor_mysql.executemany(sql_i, (val,))
                 con_mysql.commit()
+                break
 
-                # elif rfos == rfire:
-                #     if desc is None:
-                #         desc = "Sem descrição."
-                #     desc = re.sub(r"^\s+|\s+$", "", desc)
-                #     if eq is None:
-                #         eq = "Sem equipamento."
-                #     if respo is None:
-                #         respo = "Sem responsavel."
-                #     if dt_a is None or dt_a == "":
-                #         dt_a = '2021-01-14 11:11:11 1.1'
-
-                #     cursor_mysql.execute(
-                #                 f'''UPDATE core_ordem_servico
-                #                 SET
-                #                 descricao=("{desc}"),
-                #                 responsavel=("{respo}"),
-                #                 dt_agenda=({dt_a}),
-                #                 valor=(({vl})),
-                #                 equipamento=("({eq})")
-                #                 FROM core_ordem_servicos O
-                #                 INNER JOIN core_funcionario F
-                #                 ON F.referencial=({rfun})''')
+    con_mysql.close()
+    con_fire.close()
 
 except ValueError:
     print('Error database')
